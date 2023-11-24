2e4a49f86068d181f1593d54019ea7a8829ac16c
Vasant Singh Negi <Vasantnegi7@gmail.com>
2023-11-24 18:06:11 +0530

    balanced tree

---
+++ DataStructure/src/DataStructure/Trees/BalancedTree.java
@@ -0,0 +1,61 @@
+package DataStructure.Trees;
+
+/**
+ * Balanced tree
+ * Left subtree height - right subtree height >1
+ */
+
+class Node {
+
+    int data;
+    Node right;
+    Node left;
+
+    public Node(int data) {
+        this.data = data;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+public class BalancedTree {
+
+    // dry run this code
+    public static int balanceTree(Node root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int left = balanceTree(root.left);
+        int right = balanceTree(root.right);
+
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(left, right);
+
+    }
+
+    public static void main(String args[]) {
+
+        Node root = new Node(4);
+        root.left = new Node(7);
+        root.right = new Node(8);
+        root.right.right = new Node(0);
+        root.right.left = new Node(1);
+        root.right.left.left = new Node(3);
+        root.right.left.right = new Node(5);
+
+        balanceTree(root);
+
+        if (balanceTree(root) == 1) {
+            System.out.println("Balanced tree");
+        } else {
+            System.out.println("Not a balanced tree");
+        }
+
+    }
+
+}
--- DataStructure/src/DataStructure/Trees/MaxDepth.java
+++ DataStructure/src/DataStructure/Trees/MaxDepth.java
@@ -1,35 +1,59 @@
 package DataStructure.Trees;
 
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class MaxDepth {
 
-    public static void depth(TreeNode root, int count) {
+    public static int maxDepth(TreeNode root) {
 
+        // using recursion -- post order treversal
         if (root == null) {
-            return;
+            return 0;
         }
 
-        depth(root.left, count);
-        count++;
-        depth(root.right, count);
+        int leftNode = maxDepth(root.left);
+        int rightNode = maxDepth(root.right);
+
+        return 1 + Math.max(leftNode, rightNode);
+
     }
 
     public static void main(String[] args) {
 
         TreeNode root = new TreeNode(1);
-        int count = 0;
+        int ans = 0;
 
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(5);
-        root.left.right.left = new TreeNode(8);
-        root.right.left = new TreeNode(6);
-        root.right.right = new TreeNode(7);
-        root.right.right.left = new TreeNode(9);
-        root.right.right.right = new TreeNode(10);
+        // root.left.left = new TreeNode(4);
+        // root.left.right = new TreeNode(5);
+        // root.left.right.left = new TreeNode(8);
+        // root.right.left = new TreeNode(6);
+        // root.right.right = new TreeNode(7);
+        // root.right.right.left = new TreeNode(9);
+        // root.right.right.right = new TreeNode(10);
 
-        depth(root, count);
-        System.out.println(count);
+        ans = maxDepth(root);
+        System.out.println("Max depth of the tree " + ans);
 
     }
 
